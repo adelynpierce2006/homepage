ae059d47f2dbec77db47b59eb510aedf4c16d3c1
flamingpsyduck <adelynelizabethpierce@gmail.com>
2025-03-26 17:30:40 -0400

    journal entry

---
+++ jquery-lecture/main.js
@@ -0,0 +1,12 @@
+$(".dark-btn").click(
+    function(){
+        $(".square").toggleClass("dark");
+        $("body").toggleClass("dark");
+    }
+);
+
+$(".spin-btn").click(
+    function(){
+        $(".square").toggleClass
+    }
+)

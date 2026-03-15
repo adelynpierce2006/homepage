06c4bbc038792f2472ea9d68e771b9918a503d15
flamingpsyduck <adelynelizabethpierce@gmail.com>
2026-03-15 19:27:18 -0400

    .
    ver 2

---
+++ migraine-machine-website-final/index.js
@@ -0,0 +1,90 @@
+// window.location.reload()
+
+$( function() {
+    $( "#one" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#two" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#three" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#four" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#five" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#six" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#seven" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#eight" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
+
+
+$( function() {
+    $( "#nine" ).draggable();
+    $( "#ten" ).droppable({
+      drop: function( event, ui ) {
+        ui.draggable.hide();
+      }
+    });
+  } );
